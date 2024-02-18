fc8094735517b795db355919583e2cbae91743e5
Aleksei Lepekhin <aleksey.lepekhin@gmail.com>
2024-02-18 23:56:09 +0400

    Решена задача по получению из консоли двух чисел и выполнению арифметических действий

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +Math.random());
+        System.out.println("Введите первое число: ");
+        //Получаем из консоли первое число
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        //Получаем из консоли второе число
+        int secondNumber = new Scanner(System.in).nextInt();
+        //Вычисляем и выводим в косоль сумму этих чисел
+        int sum = firstNumber + secondNumber;
+        System.out.println("Сумма этих чисел: " + sum);
+        //Вычисляем и выводим в косоль разность этих чисел
+        int sub = firstNumber - secondNumber;
+        System.out.println("Разность этих чисел: " + sub);
+        //Вычисляем и выводим в косоль произведение этих чисел
+        int mul = firstNumber * secondNumber;
+        System.out.println("Произведение этих чисел: " + mul);
+        //Вычисляем и выводим в косоль частное этих чисел
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное этих чисел: " + quotient);
     }
 }
