54e189bece333af1725f22dd095ec94370ae9694
Aleksei Lepekhin <aleksey.lepekhin@gmail.com>
2024-03-31 23:58:51 +0400

    Добавил бесконечный цикл в формате while(true). Добавил использование continue в цикле

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int i = 1;
-        while (i > 0) {
+        while (true) {
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
             boolean isDirectory = file.isDirectory();
             if (!fileExists || isDirectory) {
-                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу"); continue;
-            } else {
-                System.out.println("Путь указан верно");
-                System.out.println("Это файл номер " + i++);
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу");
+                continue;
             }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + i++);
         }
     }
 }
